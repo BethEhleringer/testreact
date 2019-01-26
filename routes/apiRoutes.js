d6099eb9917b1d7d702c11a29446c3da0aae1cfc
Beth Ehleringer <elizabethehleringer@gmail.com>
2019-01-26 18:41:52 -0500

    google reading list. not working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,16 @@
 const axios = require("axios");
 const router = require("express").Router();
 
-router.get("/recipes", (req, res) => {
+router.get("/books", (req, res) => {
+  console.log("I made it to this route...")
   axios
-    .get("http://www.recipepuppy.com/api/", { params: req.query })
-    .then(({ data: { results } }) => res.json(results))
-    .catch(err => res.status(422).json(err));
+    .get("https://www.googleapis.com/books/v1/volumes?q", { params: req.query })
+    .then(({ data: { volumeInfo } }) => res.json(volumeInfo))
+    //.catch(err => res.status(422).json(err));
+    .catch(function (error) {
+      console.log(error);
+    });
+    
 });
 
 module.exports = router;
